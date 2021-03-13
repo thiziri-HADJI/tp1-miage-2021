b449b9f3cb06f9ed3c52ba03ea25ab91b9c66be7
tp <tp@tp>
2021-03-13 14:58:00 +0100

    exo 2 done 200 OK

--- todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.acme.todolist.application.port.in.AddTodoItem;
 import com.acme.todolist.application.port.in.GetTodoItems;
 import com.acme.todolist.domain.TodoItem;
 
@@ -21,11 +24,12 @@ public class TodoListController {
 	
 	
 	private GetTodoItems getTodoItemsQuery;
-	
+	private AddTodoItem addTodoItem;
 	
 	@Inject
-	public TodoListController(GetTodoItems getTodoItemsQuery ) {
+	public TodoListController(GetTodoItems getTodoItemsQuery, AddTodoItem addTodoItem ) {
 		this.getTodoItemsQuery = getTodoItemsQuery;
+		this.addTodoItem = addTodoItem; 
 	}
 	
 	@GetMapping("/todos")
@@ -33,5 +37,10 @@ public class TodoListController {
 		return this.getTodoItemsQuery.getAllTodoItems();
 	}
 	
+	@PostMapping("/todos")
+	public void ajouterItem(@RequestBody TodoItem item) {
+		this.addTodoItem.AddTodoItem(item);
+	}
+
 	
 }
